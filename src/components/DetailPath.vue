b194eeb4deb937438ef6d3950c07f64694c4b4d3
yunyou <1205361247@qq.com>
2018-10-26 16:04:23 +0800

    feat: add clockwise detection in detail-path component

--- src/components/DetailPath.vue
+++ src/components/DetailPath.vue
@@ -3,6 +3,10 @@
   </div>
 </template>
 <script type="text/javascript">
+const DIRECTION = {
+  CLOCKWISE: 0,
+  ANTI_CLOCKWISE: 1
+}
 import * as d3 from 'd3'
 export default {
   data() {
@@ -40,6 +44,23 @@ export default {
   },
   props: ['lenThreshold'],
   methods: {
+    // 判断顺逆时针需要考虑坐标系转换
+    getDirection(nodes) {
+      let len = nodes.length,
+        posNum = 0,
+        negNum = 0,
+        maxY = -1
+      nodes.forEach((val, idx) => {
+        let prevIdx = idx - 1,
+          nextIdx = (idx + 1) % len
+        prevIdx = (prevIdx < 0) ? (prevIdx + len) % len : prevIdx
+        let prevNode = nodes[prevIdx],
+          nextNode = nodes[nextIdx]
+        if ((val.x - prevNode.x) * (val.y - nextNode.y) - (prevNode.y - val.y) * (nextNode.x - val.x) > 0) posNum++
+          else negNum++
+      })
+      return posNum > negNum ? DIRECTION.ANTI_CLOCKWISE : DIRECTION.CLOCKWISE
+    },
     genRelPath(path) {
       let match = path.match(/\/Users\/wendahuang\/Desktop\/vue\/src\/(.*)/)
       return match ? match[1] : path
@@ -48,11 +69,10 @@ export default {
       d3.select(this.$refs.root).selectAll('svg *').remove()
       let vm = this
       // 添加箭头形状
-      this.svg.append("defs").append("marker")
+      let arrowMarker=this.svg.append("defs").append("marker")
         .attr("id", "detail-path-arrow")
         .attr("viewBox", "0 -5 10 10")
         .attr("refX", 15)
-        .attr("refY", -1.5)
         .attr("markerWidth", 3)
         .attr("markerHeight", 3)
         .attr("orient", "auto")
@@ -115,20 +135,33 @@ export default {
         .enter().append("text")
         .attr("x", 8)
         .attr("y", ".31em")
+        // .attr("text-anchor","middle")
         .text(function(d) { return vm.formatText(d.id); });
       // 边文字信息
       this.svg.append("g").selectAll(".link-text")
         .data()
       simulation
         .nodes(this.graphData.nodes)
-        .on("tick", ticked);
+        .on("tick", ticked)
+        .on("end", (d) => {
+/*          console.log(vm.nodes.nodes(), vm.nodes, vm.graphData.nodes)
+          console.log(d3.select())
+          vm.nodes.each((d) => {
+            console.log(d)
+          })
+          console.log(vm.getDirection(vm.graphData.nodes))*/
+        });
 
       simulation.force("link")
         .links(this.graphData.links);
 
-
       function ticked() {
-        vm.linksPath.attr("d", linkArc);
+        let direction = DIRECTION.CLOCKWISE
+        if (vm.type === 'indirect' || vm.type === 'direct') {
+          direction = vm.getDirection(vm.graphData.nodes)
+        }
+        arrowMarker.attr("refY", direction===DIRECTION.CLOCKWISE?-1.5:1.5)
+        vm.linksPath.attr("d", getDirectArcPath(direction));
         vm.nodes.attr("transform", transform);
         vm.text.attr("transform", transform);
       }
@@ -150,12 +183,17 @@ export default {
         d.fy = null;
       }
 
-      function linkArc(d) {
-        var dx = d.target.x - d.source.x,
-          dy = d.target.y - d.source.y,
-          dr = Math.sqrt(dx * dx + dy * dy);
-        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+      function getDirectArcPath(direction) {
+        return function linkArc(d) {
+          var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+          let rotateDir=direction===DIRECTION.CLOCKWISE?1:0
+          // let rotateDir=1
+          return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + ` 0 0,${rotateDir} ` + d.target.x + "," + d.target.y;
+        }
       }
+
 
       function transform(d) {
         return "translate(" + d.x + "," + d.y + ")";
